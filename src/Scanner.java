ac60ad4c77636ae1c91d721a6ee43a614d1d8461
Italo Guajardo <italog.penaloza@gmail.com>
2022-08-22 19:40:24 -0400

    check if is number

--- src/Scanner.java
+++ src/Scanner.java
@@ -101,8 +101,37 @@ class Scanner {
                 break;
 
             default:
-                Jami.error(line, "Unexpected character.");
+                if (isDigit(c)) {
+                    number();
+                } else {
+                    Jami.error(line, "Unexpected character.");
+                }
+                break;
         }
+    }
+
+    private boolean isDigit(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek()))
+            advance();
+
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+            while (isDigit(peek()))
+                advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+    }
+
+    private char peekNext() {
+        if (current + 1 >= source.length())
+            return '\0';
+        return source.charAt(current + 1);
     }
 
     private void string() {
