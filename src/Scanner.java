44d116b5d27dd01198e57c6cfcd9f07404660abc
Italo Guajardo <italog.penaloza@gmail.com>
2022-08-22 19:52:57 -0400

    check if is identifier

--- src/Scanner.java
+++ src/Scanner.java
@@ -103,11 +103,29 @@ class Scanner {
             default:
                 if (isDigit(c)) {
                     number();
+                } else if (isAlpha(c)) {
+                    identifier();
                 } else {
                     Jami.error(line, "Unexpected character.");
                 }
+
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        addToken(TokenType.IDENTIFIER);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
     }
 
     private boolean isDigit(char c) {
